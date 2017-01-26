407019d8d65bbc163b78c880d64317809a4b2eae
nbourre <nbourrereg@hotmail.com>
2017-01-25 19:45:35 -0500

    Premier commit

---
+++ labo_01_convertisseur/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace labo_01_convertisseur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nudDecimal = new System.Windows.Forms.NumericUpDown();
+            this.nudBin03 = new System.Windows.Forms.NumericUpDown();
+            this.nudBin02 = new System.Windows.Forms.NumericUpDown();
+            this.nudBin00 = new System.Windows.Forms.NumericUpDown();
+            this.nudBin01 = new System.Windows.Forms.NumericUpDown();
+            this.nudHex = new System.Windows.Forms.NumericUpDown();
+            this.chkCounter = new System.Windows.Forms.CheckBox();
+            this.radDec = new System.Windows.Forms.RadioButton();
+            this.radBinaire = new System.Windows.Forms.RadioButton();
+            this.radHex = new System.Windows.Forms.RadioButton();
+            this.lblSeg_A = new System.Windows.Forms.Label();
+            this.lblSeg_G = new System.Windows.Forms.Label();
+            this.lblSeg_D = new System.Windows.Forms.Label();
+            this.lblSeg_B = new System.Windows.Forms.Label();
+            this.lblSeg_C = new System.Windows.Forms.Label();
+            this.lblSeg_E = new System.Windows.Forms.Label();
+            this.lblSeg_F = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDecimal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudDecimal
+            // 
+            this.nudDecimal.Location = new System.Drawing.Point(12, 38);
+            this.nudDecimal.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nudDecimal.Name = "nudDecimal";
+            this.nudDecimal.Size = new System.Drawing.Size(41, 20);
+            this.nudDecimal.TabIndex = 0;
+            this.nudDecimal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudDecimal.ValueChanged += new System.EventHandler(this.nudDecimal_ValueChanged);
+            // 
+            // nudBin03
+            // 
+            this.nudBin03.Location = new System.Drawing.Point(119, 38);
+            this.nudBin03.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBin03.Name = "nudBin03";
+            this.nudBin03.Size = new System.Drawing.Size(41, 20);
+            this.nudBin03.TabIndex = 1;
+            this.nudBin03.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBin03.ValueChanged += new System.EventHandler(this.nudBin_ValueChanged);
+            // 
+            // nudBin02
+            // 
+            this.nudBin02.Location = new System.Drawing.Point(166, 38);
+            this.nudBin02.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBin02.Name = "nudBin02";
+            this.nudBin02.Size = new System.Drawing.Size(41, 20);
+            this.nudBin02.TabIndex = 2;
+            this.nudBin02.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBin02.ValueChanged += new System.EventHandler(this.nudBin_ValueChanged);
+            // 
+            // nudBin00
+            // 
+            this.nudBin00.Location = new System.Drawing.Point(260, 38);
+            this.nudBin00.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBin00.Name = "nudBin00";
+            this.nudBin00.Size = new System.Drawing.Size(41, 20);
+            this.nudBin00.TabIndex = 4;
+            this.nudBin00.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBin00.ValueChanged += new System.EventHandler(this.nudBin_ValueChanged);
+            // 
+            // nudBin01
+            // 
+            this.nudBin01.Location = new System.Drawing.Point(213, 38);
+            this.nudBin01.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBin01.Name = "nudBin01";
+            this.nudBin01.Size = new System.Drawing.Size(41, 20);
+            this.nudBin01.TabIndex = 3;
+            this.nudBin01.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBin01.ValueChanged += new System.EventHandler(this.nudBin_ValueChanged);
+            // 
+            // nudHex
+            // 
+            this.nudHex.Hexadecimal = true;
+            this.nudHex.Location = new System.Drawing.Point(372, 38);
+            this.nudHex.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nudHex.Name = "nudHex";
+            this.nudHex.Size = new System.Drawing.Size(41, 20);
+            this.nudHex.TabIndex = 5;
+            this.nudHex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudHex.ValueChanged += new System.EventHandler(this.nudHex_ValueChanged);
+            // 
+            // chkCounter
+            // 
+            this.chkCounter.AutoSize = true;
+            this.chkCounter.Location = new System.Drawing.Point(166, 366);
+            this.chkCounter.Name = "chkCounter";
+            this.chkCounter.Size = new System.Drawing.Size(71, 17);
+            this.chkCounter.TabIndex = 6;
+            this.chkCounter.Text = "Compteur";
+            this.chkCounter.UseVisualStyleBackColor = true;
+            this.chkCounter.CheckedChanged += new System.EventHandler(this.chkCounter_CheckedChanged);
+            // 
+            // radDec
+            // 
+            this.radDec.AutoSize = true;
+            this.radDec.Location = new System.Drawing.Point(12, 15);
+            this.radDec.Name = "radDec";
+            this.radDec.Size = new System.Drawing.Size(63, 17);
+            this.radDec.TabIndex = 7;
+            this.radDec.TabStop = true;
+            this.radDec.Text = "Décimal";
+            this.radDec.UseVisualStyleBackColor = true;
+            this.radDec.CheckedChanged += new System.EventHandler(this.radDec_CheckedChanged);
+            // 
+            // radBinaire
+            // 
+            this.radBinaire.AutoSize = true;
+            this.radBinaire.Location = new System.Drawing.Point(119, 12);
+            this.radBinaire.Name = "radBinaire";
+            this.radBinaire.Size = new System.Drawing.Size(57, 17);
+            this.radBinaire.TabIndex = 8;
+            this.radBinaire.TabStop = true;
+            this.radBinaire.Text = "Binaire";
+            this.radBinaire.UseVisualStyleBackColor = true;
+            this.radBinaire.CheckedChanged += new System.EventHandler(this.radBinaire_CheckedChanged);
+            // 
+            // radHex
+            // 
+            this.radHex.AutoSize = true;
+            this.radHex.Location = new System.Drawing.Point(372, 12);
+            this.radHex.Name = "radHex";
+            this.radHex.Size = new System.Drawing.Size(44, 17);
+            this.radHex.TabIndex = 9;
+            this.radHex.TabStop = true;
+            this.radHex.Text = "Hex";
+            this.radHex.UseVisualStyleBackColor = true;
+            this.radHex.CheckedChanged += new System.EventHandler(this.radHex_CheckedChanged);
+            // 
+            // lblSeg_A
+            // 
+            this.lblSeg_A.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_A.Location = new System.Drawing.Point(154, 118);
+            this.lblSeg_A.Name = "lblSeg_A";
+            this.lblSeg_A.Size = new System.Drawing.Size(100, 25);
+            this.lblSeg_A.TabIndex = 10;
+            // 
+            // lblSeg_G
+            // 
+            this.lblSeg_G.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_G.Location = new System.Drawing.Point(154, 197);
+            this.lblSeg_G.Name = "lblSeg_G";
+            this.lblSeg_G.Size = new System.Drawing.Size(100, 25);
+            this.lblSeg_G.TabIndex = 11;
+            // 
+            // lblSeg_D
+            // 
+            this.lblSeg_D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_D.Location = new System.Drawing.Point(154, 276);
+            this.lblSeg_D.Name = "lblSeg_D";
+            this.lblSeg_D.Size = new System.Drawing.Size(100, 25);
+            this.lblSeg_D.TabIndex = 12;
+            // 
+            // lblSeg_B
+            // 
+            this.lblSeg_B.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_B.Location = new System.Drawing.Point(260, 128);
+            this.lblSeg_B.Name = "lblSeg_B";
+            this.lblSeg_B.Size = new System.Drawing.Size(25, 75);
+            this.lblSeg_B.TabIndex = 13;
+            // 
+            // lblSeg_C
+            // 
+            this.lblSeg_C.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_C.Location = new System.Drawing.Point(260, 216);
+            this.lblSeg_C.Name = "lblSeg_C";
+            this.lblSeg_C.Size = new System.Drawing.Size(25, 75);
+            this.lblSeg_C.TabIndex = 14;
+            // 
+            // lblSeg_E
+            // 
+            this.lblSeg_E.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_E.Location = new System.Drawing.Point(123, 216);
+            this.lblSeg_E.Name = "lblSeg_E";
+            this.lblSeg_E.Size = new System.Drawing.Size(25, 75);
+            this.lblSeg_E.TabIndex = 16;
+            // 
+            // lblSeg_F
+            // 
+            this.lblSeg_F.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeg_F.Location = new System.Drawing.Point(123, 128);
+            this.lblSeg_F.Name = "lblSeg_F";
+            this.lblSeg_F.Size = new System.Drawing.Size(25, 75);
+            this.lblSeg_F.TabIndex = 15;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 439);
+            this.Controls.Add(this.lblSeg_E);
+            this.Controls.Add(this.lblSeg_F);
+            this.Controls.Add(this.lblSeg_C);
+            this.Controls.Add(this.lblSeg_B);
+            this.Controls.Add(this.lblSeg_D);
+            this.Controls.Add(this.lblSeg_G);
+            this.Controls.Add(this.lblSeg_A);
+            this.Controls.Add(this.radHex);
+            this.Controls.Add(this.radBinaire);
+            this.Controls.Add(this.radDec);
+            this.Controls.Add(this.chkCounter);
+            this.Controls.Add(this.nudHex);
+            this.Controls.Add(this.nudBin00);
+            this.Controls.Add(this.nudBin01);
+            this.Controls.Add(this.nudBin02);
+            this.Controls.Add(this.nudBin03);
+            this.Controls.Add(this.nudDecimal);
+            this.Name = "Form1";
+            this.Text = "Convertisseur visuel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDecimal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBin01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudDecimal;
+        private System.Windows.Forms.NumericUpDown nudBin03;
+        private System.Windows.Forms.NumericUpDown nudBin02;
+        private System.Windows.Forms.NumericUpDown nudBin00;
+        private System.Windows.Forms.NumericUpDown nudBin01;
+        private System.Windows.Forms.NumericUpDown nudHex;
+        private System.Windows.Forms.CheckBox chkCounter;
+        private System.Windows.Forms.RadioButton radDec;
+        private System.Windows.Forms.RadioButton radBinaire;
+        private System.Windows.Forms.RadioButton radHex;
+        private System.Windows.Forms.Label lblSeg_A;
+        private System.Windows.Forms.Label lblSeg_G;
+        private System.Windows.Forms.Label lblSeg_D;
+        private System.Windows.Forms.Label lblSeg_B;
+        private System.Windows.Forms.Label lblSeg_C;
+        private System.Windows.Forms.Label lblSeg_E;
+        private System.Windows.Forms.Label lblSeg_F;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ labo_01_convertisseur/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace labo_01_convertisseur
+{
+
+    public partial class Form1 : Form
+    {
+        private Label[] septSegments = new Label[7];
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nudDecimal_ValueChanged(object sender, EventArgs e)
+        {
+            if (radDec.Checked)
+            {
+                if (nudDecimal.Value > nudDecimal.Maximum)
+                {
+                    nudDecimal.Value = 0;
+                }
+                nudHex.Value = nudDecimal.Value;
+                AffecteValeurBinaire((int)nudDecimal.Value);
+                Affiche7Segments((int)nudDecimal.Value);
+                
+            }
+        }
+
+        private void nudHex_ValueChanged(object sender, EventArgs e)
+        {
+            if (radHex.Checked)
+            {
+                nudDecimal.Value = nudHex.Value;
+                AffecteValeurBinaire((int)nudDecimal.Value);
+                Affiche7Segments((int)nudDecimal.Value);
+            }
+        }
+
+        private void nudBin_ValueChanged(object sender, EventArgs e)
+        {
+            if (radBinaire.Checked)
+            {
+                binaire2Integer();
+                Affiche7Segments((int)nudDecimal.Value);
+            }
+        }
+
+        private void radDec_CheckedChanged(object sender, EventArgs e)
+        {
+            nudDecimal.Enabled = radDec.Checked;
+        }
+
+        private void radBinaire_CheckedChanged(object sender, EventArgs e)
+        {
+            nudBin00.Enabled = nudBin01.Enabled = nudBin02.Enabled = nudBin03.Enabled = radBinaire.Checked;
+        }
+
+        private void radHex_CheckedChanged(object sender, EventArgs e)
+        {
+            nudHex.Enabled = radHex.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            septSegments[0] = lblSeg_A;
+            septSegments[1] = lblSeg_B;
+            septSegments[2] = lblSeg_C;
+            septSegments[3] = lblSeg_D;
+            septSegments[4] = lblSeg_E;
+            septSegments[5] = lblSeg_F;
+            septSegments[6] = lblSeg_G;
+
+
+            radDec.Checked = true;
+            radBinaire.Checked = nudBin00.Enabled = nudBin01.Enabled = nudBin02.Enabled = nudBin03.Enabled = false;
+            radHex.Checked = nudHex.Enabled = false;
+
+            nudDecimal.Value = 0;
+        }
+
+        private void AffecteValeurBinaire(int valeur)
+        {
+            nudBin00.Value = valeur % 2;
+            valeur >>= 1;
+            nudBin01.Value = valeur % 2;
+            valeur >>= 1;
+            nudBin02.Value = valeur % 2;
+            valeur >>= 1;
+            nudBin03.Value = valeur % 2;
+        }
+
+        private void binaire2Integer()
+        {
+            int result = (int)(nudBin00.Value + 2 * nudBin01.Value + 4 * nudBin02.Value + 8 * nudBin03.Value);
+            nudDecimal.Value = nudHex.Value = result;
+        }
+
+        #region Section de code pour l'affichage du 7 segments
+
+        // Table de vérité (valeur,A,B,C,D,E,F,G) pour l'affichage des segments
+        private int[,] LS7447 = new int[,]
+            { //0 1 2 3 4 5 6
+                {1,1,1,1,1,1,0}, // 0
+                {0,1,1,0,0,0,0}, // 1
+                {1,1,0,1,1,0,1}, // 2
+                {1,1,1,1,0,0,1}, // 3
+    
+                {0,1,1,0,0,1,1}, // 4
+                {1,0,1,1,0,1,1}, // 5
+                {1,0,1,1,1,1,1}, // 6
+                {1,1,1,0,0,0,0}, // 7
+
+                {1,1,1,1,1,1,1}, // 8
+                {1,1,1,0,0,1,1}, // 9
+                {1,1,1,0,1,1,1}, // A
+                {0,0,1,1,1,1,1}, // B
+
+                {1,0,0,1,1,1,0}, // C
+                {0,1,1,1,1,0,1}, // D
+                {1,0,0,1,1,1,1}, // E
+                {1,0,0,0,1,1,1}  // F
+            };
+
+        /// <summary>
+        /// Affiche la valeur sur le pseudo 7 segments représenté par 7 Libellés
+        /// </summary>
+        /// <param name="valeur">La valeur à afficher (0-15).</param>
+        private void Affiche7Segments(int valeur)
+        {
+            for (int i = 0; i < 7; ++i)
+                if (LS7447[valeur, i] == 1)
+                    septSegments[i].BackColor = Color.Red;
+                else
+                    septSegments[i].BackColor = Color.LightGray;
+        }
+
+        #endregion
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nudDecimal.Value = (nudDecimal.Value + 1) % (nudDecimal.Maximum + 1);
+        }
+
+        private void chkCounter_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = chkCounter.Checked;
+        }
+    }
+}
